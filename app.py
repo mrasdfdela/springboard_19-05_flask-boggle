d1ff6415c8b2cd9d853d592f7d8c64fb4625e609
Jerry Hsu <Jerry>
2021-05-07 23:39:39 -0400

    Completed Step 2: Displaying the Board

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from boggle import Boggle
 from flask import Flask, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 # app.debug = True
 
-boggle_game = Boggle()
 
 @app.route('/')
 def home_page():
-    return '<h1>Hello world!</h1>'
+    boggle_game = Boggle()
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template("index.html", board=session['board'])
