7581a8424bc4fdf06654ea12ea512796ab3822c5
Jerry Hsu <Jerry>
2021-05-07 23:14:57 -0400

    Completed Step 1: Planning and Reading Code

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+debug = DebugToolbarExtension(app)
+# app.debug = True
 
 boggle_game = Boggle()
+
+@app.route('/')
+def home_page():
+    return '<h1>Hello world!</h1>'
---
+++ test_app.py
@@ -0,0 +1,5 @@
+# from app import app
+
+# app.config['TESTING'] = True
+# app.config[DEBUG_TB_HOSTS] = ['dont-show-debug-toolbar']
+
